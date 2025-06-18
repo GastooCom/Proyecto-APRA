608538aaf9f0cf68620a9980bdfd5e4832180ffb
lemaov <lemajoaquin3@gmail.com>
2025-06-17 23:59:33 -0300

    al agregar el formulario con las animaciones se descontrolo asqieu se corrijio

--- proyecto-apra/src/components/FormularioRegistrarse.jsx
+++ proyecto-apra/src/components/FormularioRegistrarse.jsx
@@ -3,72 +3,74 @@ import "../css/FormularioRegistrarse.css";
 
 const FormularioRegistrarse = () => {
   return (
-    <div className="container">
-      <div className="form_area">
-        <p className="title">Regístrate</p>
-        <form action="#" method="post" autoComplete="on">
-          {/* Nombre */}
-          <div className="form_group">
-            <label className="sub_title" htmlFor="name">
-              <i className="fas fa-user"></i> Nombre completo
-            </label>
-            <input
-              className="form_style"
-              type="text"
-              id="name"
-              name="name"
-              placeholder="Ingrese su nombre completo"
-              required
-              minLength={3}
-              maxLength={50}
-            />
-          </div>
+    <div className="pagina-registro">
+      <div className="contenedor-registro">
+        <div className="form_area">
+          <p className="titulo-registrarse">Regístrate</p>
+          <form action="#" method="post" autoComplete="on">
+            {/* Nombre */}
+            <div className="form_group">
+              <label className="sub_title" htmlFor="name">
+                <i className="fas fa-user"></i> Nombre completo
+              </label>
+              <input
+                className="form_style"
+                type="text"
+                id="name"
+                name="name"
+                placeholder="Ingrese su nombre completo"
+                required
+                minLength={3}
+                maxLength={50}
+              />
+            </div>
 
-          {/* Teléfono */}
-          <div className="form_group">
-            <label className="sub_title" htmlFor="telefono">
-              <i className="fas fa-phone"></i> Nº de Teléfono
-            </label>
-            <input
-              className="form_style"
-              type="tel"
-              id="telefono"
-              name="telefono"
-              placeholder="Ej: 2991234567"
-              pattern="[0-9]{10}"
-              required
-            />
-          </div>
+            {/* Teléfono */}
+            <div className="form_group">
+              <label className="sub_title" htmlFor="telefono">
+                <i className="fas fa-phone"></i> Nº de Teléfono
+              </label>
+              <input
+                className="form_style"
+                type="tel"
+                id="telefono"
+                name="telefono"
+                placeholder="Ej: 2991234567"
+                pattern="[0-9]{10}"
+                required
+              />
+            </div>
 
-          {/* DNI */}
-          <div className="form_group">
-            <label className="sub_title" htmlFor="dni">
-              <i className="fas fa-id-card"></i> DNI
-            </label>
-            <input
-              className="form_style"
-              type="text"
-              id="dni"
-              name="dni"
-              placeholder="Ingrese su DNI"
-              pattern="[0-9]{7,8}"
-              required
-            />
-          </div>
+            {/* DNI */}
+            <div className="form_group">
+              <label className="sub_title" htmlFor="dni">
+                <i className="fas fa-id-card"></i> DNI
+              </label>
+              <input
+                className="form_style"
+                type="text"
+                id="dni"
+                name="dni"
+                placeholder="Ingrese su DNI"
+                pattern="[0-9]{7,8}"
+                required
+              />
+            </div>
 
-          {/* Botón */}
-          <button className="btn" type="submit">
-            Registrarse
-          </button>
+            {/* Botón */}
+            <button className="btn" type="submit">
+              Registrarse
+            </button>
 
-          {/* Pie */}
-          <p className="texto-pie">
-            ¿Ya tienes una Cuenta?{" "}
-            <a className="link" href="#">
-              Inicia con Google
-            </a>
-          </p>
-        </form>
+            {/* Pie */}
+            <p className="texto-pie">
+              ¿Ya tienes una Cuenta?{" "}
+              <a className="link" href="#">
+                Inicia con Google
+              </a>
+            </p>
+          </form>
+        </div>
       </div>
     </div>
   );
