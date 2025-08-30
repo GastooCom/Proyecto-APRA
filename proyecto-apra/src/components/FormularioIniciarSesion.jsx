d34b4d6e9065353cf7502cc5b9645cc278c2fedb
FacundoCarrasco <facundocarrasco509@gmail.com>
2025-08-30 19:12:44 -0300

    Agregando el boton para volver al inicio

--- proyecto-apra/src/components/FormularioIniciarSesion.jsx
+++ proyecto-apra/src/components/FormularioIniciarSesion.jsx
@@ -9,6 +9,13 @@ const FormularioIniciarSesion = () => {
 
   return (
     <div className="caja-iniciosesion">
+      <button className="boton-volver" onClick={() => navigate("/")}>
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+               fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+            <path d="M19 12H5"></path>
+            <path d="M12 19l-7-7 7-7"></path>
+          </svg>
+        </button>
     <div className="caja-login">
       <h1>Iniciar Sesión</h1>
 
